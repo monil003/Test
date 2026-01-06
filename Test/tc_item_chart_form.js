27dd214ef24abe714999c53df3015b0a3a78fd35
monil003 <mdumasia@oncloudconsulting.ca>
2026-01-06 16:46:50 -0500

    NetSuite sync tc_item_chart_form.js (v1.0.1)

---
+++ Test/tc_item_chart_form.js
@@ -0,0 +1,48 @@
+/**
+ * @version v1.0.1
+ */
+
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define(['N/ui/serverWidget', 'N/search'], function (serverWidget, search) {
+
+
+    function beforeLoad(context) {
+        try {
+            if (context.type !== context.UserEventType.VIEW) return;
+
+
+            var form = context.form;
+            const itemId = context.newRecord.id;
+            const locationId = null;
+
+
+            var htmlField = form.addField({
+                id: 'custpage_chart_html',
+                label: ' ',
+                type: serverWidget.FieldType.INLINEHTML,
+                container: 'custom777'
+            });
+
+
+            const inventoryData = getAssemblyItemInventory(itemId);
+            // const soUsageData = getSOUsage(itemId, locationId);
+            const soUsageData = getSOUsage1(itemId, locationId);
+
+
+            log.debug('inventoryData', inventoryData);
+            log.debug('soUsageData', soUsageData);
+
+
+            htmlField.defaultValue = generateAssemblyHtml(inventoryData, soUsageData);
+
+
+        } catch (error) {
+            log.debug('error', error);
+        }
+    }
+
+
+    function generateAssemblyHtml(inventoryData, soUsageData) {
