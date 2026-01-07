dc1339a93d003a6eb935db2394ac5154f88cef59
monil003 <mdumasia@oncloudconsulting.ca>
2026-01-07 14:03:51 -0500

    NetSuite sync avalara_product_integration.js (v1.0.1)

--- Test/avalara_product_integration.js
+++ Test/avalara_product_integration.js
@@ -2,6 +2,55 @@
  * @version v1.0.1
  */
 
+
+
+            log.debug('params', params);
+
+
+            // --------------------------------------------
+            // Avalara Integration fields
+            // --------------------------------------------
+            const avalaraFields = [
+                'upccode',
+                'displayname',
+                'itemid',
+                'custitem_tusa_unit_capacity',
+                'custitemtusa_brand_description',
+                'custitemmdm_rsku',
+                'cost',
+                'custitem8',
+            ];
+
+
+            // --------------------------------------------
+            // On EDIT: Only trigger if at least one changed
+            // --------------------------------------------
+            if (context.type === context.UserEventType.EDIT && oldRec) {
+                const changed = avalaraFields.some(fieldId => {
+                    return newRec.getValue(fieldId) !== oldRec.getValue(fieldId);
+                });
+
+
+                if (!changed) {
+                    log.debug('Avalara Sync Skipped', 'No relevant field changes detected.');
+                    // return;
+                }
+            }
+
+
+
+
+            const itemRec = newRec;
+            const itemId = itemRec.id;
+            const itemName = itemRec.getValue('itemid');
+            const itemCategoryId = newRec.getValue('custitemcategory');
+            const displayName = itemRec.getValue('displayname');
+            // const customId = displayName ? displayName.replace(/\s+/g, '_') : '';
+            const customId = String(itemName);
+            const upcCode = itemRec.getValue('upccode');
+            const unitCapacity = itemRec.getValue('custitem_tusa_unit_capacity');
+            const brandDesc = itemRec.getValue('custitemtusa_brand_description');
+            const manufacturerCode = itemRec.getValue('custitemmdm_rsku');
             const productAttributeValue1 = itemRec.getValue('cost');
             const productAttributeValue2 = itemRec.getSublistValue({ sublistId: 'price1', fieldId: 'price_1_', line: 0 });
             const productAttributeValue3 = itemRec.getValue('custitem8');
@@ -41,68 +90,3 @@
                 const headers = {
                     "Content-Type": "application/json",
                     "x-company-id": "1534",
-                    "Authorization": "Basic bnNhcGl1c2VyOkFsdGFkaXN1c2E1OTAwIQ==",
-                    "Accept": "application/json"
-                };
-
-
-                log.debug('payload', payload);
-
-
-                const url = "https://excisesbx.avalara.com/api/v1/AlternateProducts/Create";
-
-
-                const response = https.post({
-                    url: url,
-                    body: JSON.stringify(payload),
-                    headers: headers
-                });
-
-
-                log.debug('response', response);
-
-
-                if (response.code === 200) {
-                    const resBody = JSON.parse(response.body);
-                    
-                    log.audit('Avalara Tobacco API Response', {
-                      code: response.code,
-                      body: resBody
-                    });
-                } else {
-                    
-                    if (response.code == 400 && response.body == 'A matching record already exists in the system.') {
-                       const url = "https://excisesbx.avalara.com/api/v1/AlternateProducts/Update";
-
-
-                       const updateResponse = https.put({
-                          url: url,
-                          body: JSON.stringify(payload),
-                          headers: headers
-                       });
-
-
-                       log.debug('updateResponse', updateResponse);
-                    } else {
-                      log.audit('Avalara Alternate product Result failed', response.body);
-                    }
-                }
-
-
-               return;
-            }
-
-
-            // -------------------------------
-            // Build Payload
-            // -------------------------------
-            const payload = {
-                TobaccoProducts: [
-                    {
-                        CountryCode: params.custscript_country_code,
-                        Jurisdiction: params.custscript_juridiction,
-                        Upc: upcCode,
-                        Name: displayName,
-                        ProductCode: "CIGAR",
-                        CustomId: itemName ? itemName : customId,
-                        EffectiveDate: params.custscript_effective_date,
