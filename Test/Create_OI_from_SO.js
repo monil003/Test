3639e713431acdd6a170730b1e7747db8b458f97
monil003 <mdumasia@oncloudconsulting.ca>
2026-01-08 17:06:28 -0500

    NetSuite sync Create_OI_from_SO.js (v1.0.1)

--- Test/Create_OI_from_SO.js
+++ Test/Create_OI_from_SO.js
@@ -26,7 +26,7 @@ define(['N/record','N/log','N/search', 'N/error','N/runtime','N/format'],
 function(record, log, search, error,runtime,format) {
   function afterSubmit(context) {
     
-    //test123 test1 test
+    //test123 test1 test2
     log.debug({title:'context',details:context})
     var executioncontext;
     executioncontext = runtime.executionContext;
@@ -38,3 +38,30 @@ function(record, log, search, error,runtime,format) {
           Sales_Order: 1,
           Consignment:2,
           RD_Trial:5,
+          Intercompany_Transfer:6,
+          Subcontracted:7,
+          For_Inventory:8,
+          RMA	:9,
+          Non_Inventory_Return:10
+        }
+        
+        var ORDER_REASON = {
+          Sales_Order:1,
+          Forecast:2,
+          Advance_Replacement:3,
+          Samples:4,
+          Donation:5,
+          VMI_Sale:6,
+          Team_Member_Order:7,
+          Transfer_Order:8,
+          Product_Issue:9,
+          Backorder_Cancellation:10,
+          Pricing_issue_Credit_Rebill:11,
+          Product_Sunset:12,
+          Shipping_Issue:13,
+          Warranty_Return:14,
+          Approved_Customer_Return:15,
+          Order_Entry_Issue:16,
+          Customer_Issue:17,
+          Product_Sunset:18,
+          Shipping_Issue:19
