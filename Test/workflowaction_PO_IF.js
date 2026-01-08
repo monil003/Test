312344b8731e9641fb665805d117033b36c34bfe
monil003 <mdumasia@oncloudconsulting.ca>
2026-01-08 14:36:50 -0500

    NetSuite sync workflowaction_PO_IF.js (v1.0.1)

---
+++ Test/workflowaction_PO_IF.js
@@ -0,0 +1,48 @@
+/**
+ * @version v1.0.1
+ */
+
+/**
+* @NApiVersion 2.x
+* @NScriptType WorkflowActionScript
+* @NModuleScope SameAccount
+*/
+define(['N/render', 'N/file', 'N/record', 'N/email','N/search','N/runtime'], function(render, file, record, email,search,runtime){
+  function onAction(scriptContext){
+
+
+
+
+    var purchaseOrder = scriptContext.newRecord.id;
+    log.debug({title:'purchaseOrder',details:purchaseOrder})
+    var type = scriptContext.newRecord.type;
+    log.debug({title:'type',details:type})
+
+
+    try{
+
+
+
+
+    }
+    catch(error){
+      if (error instanceof nlobjError) {
+        var errorMsg = "Code: " + error.getCode() + " Details: " + error.getDetails();
+        log.error('An error occurred.', errorMsg);
+      }
+      else {
+        log.error('An unknown error occurred.', error.toString());
+      }
+    }
+  }
+
+
+
+
+
+
+  function isEmpty(value) {
+    if (value === null) {
+      return true;
+    } else if (value === undefined) {
+      return true;
