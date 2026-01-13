07dfda610aadb4ac787a3b70b1ebd8266247f723
monil003 <mdumasia@oncloudconsulting.ca>
2026-01-13 09:58:14 -0500

    NetSuite sync bpc_pushpay_webhook.js (v1.0.1)

---
+++ Test/bpc_pushpay_webhook.js
@@ -0,0 +1,42 @@
+/**
+ * @version v1.0.1
+ */
+
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/log', 'N/record'], (log, record) => {
+
+
+    const onRequest = (context) => {
+        log.debug('here inn', context);
+        if (context.request.method !== 'POST') {
+            context.response.write('Method Not Allowed');
+            return;
+        }
+
+
+        // Validate shared secret
+        const secret = context.request.headers['x-pushpay-secret'];
+        if (secret !== 'MY_SHARED_SECRET') {
+            context.response.write('Unauthorized');
+            return;
+        }
+
+
+        const body = JSON.parse(context.request.body);
+        log.audit('Pushpay Webhook', body);
+
+
+        // Minimal processing here
+        // Queue Map/Reduce or save to custom record
+
+
+        context.response.write('OK');
+    };
+
+
+    return { onRequest };
+});
+
