5dad8d1760a49a74c9faeedd1063f2c93dd729ba
monil003 <mdumasia@oncloudconsulting.ca>
2026-01-16 10:30:50 -0500

    NetSuite sync schedule_shipment_MR_v3.js (v1.0.1)

--- Test/schedule_shipment_MR_v3.js
+++ Test/schedule_shipment_MR_v3.js
@@ -386,6 +386,12 @@ function mapReduce(error, record, runtime, render, email, search, task, format) 
                     fieldId: 'custcol_tc_scheduled_ship_date',
                     line: 0
                 });
+
+                if (obj.shippingCost) {
+                  load_sales_order.setValue({ fieldId: 'custbody_tc_sampleshippingcost', value: obj.shippingCost });
+                }
+
+                load_sales_order.save({ enableSourcing: true, ignoreMandatoryFields: true });
             } catch (error) {
                 log.debug('error in schedule ship date fetching');
             }
