2388b9d10a9dce70d1910d83465aae35f8a13ad0
monil003 <mdumasia@oncloudconsulting.ca>
2026-02-04 17:21:43 -0500

    [Auto] test

---
+++ SetAssemblyUpdatedToTrue.js
@@ -0,0 +1,49 @@
+/**
+*@NApiVersion 2.x
+*@NScriptType ScheduledScript
+*/
+​
+/*
+**********************************************************************
+***********************************************************************
+* Script Description:
+* This is a scheduled script that runs once every night to chnage Assembly Buid Update checkbox to TRUE
+* Record Type = customrecord1503//Build Qty Update
+***********************************************************************
+*/
+​
+define(['N/log','N/search','N/record'],
+function(log,search,record){
+  function updateBuildCheckbox(scriptContext){
+
+    //test
+
+    var customrecord1503SearchObj = search.create({
+      type: "customrecord1503",
+      filters:
+      [
+        ["custrecord_tc_assembly_build","is","F"]
+      ],
+      columns:
+      [
+        search.createColumn({name: "internalid", label: "Internal ID"})
+      ]
+    });
+    var searchResultCount = customrecord1503SearchObj.runPaged().count;
+    log.debug("customrecord1503SearchObj result count",searchResultCount);
+    customrecord1503SearchObj.run().each(function(result){
+
+      var id = result.getValue({name:'internalid'})
+
+      var loadedRecord = record.load({type:'customrecord1503',id:id,isDynamic:true})
+      //loadedRecord.setValue({fieldId:'custrecord_tc_assembly_build',value:true})
+      loadedRecord.save()
+      //record.submitFields({type:'customrecord1503',id:id,id,values:{custrecord_tc_assembly_build:true}})
+
+      return true;
+    });  ​
+  }
+  return{
+    execute: updateBuildCheckbox
+  };
+})
